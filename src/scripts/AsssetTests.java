f084aed1517a34f7be16cfcae67492d5bea106c3
Al Braza <ABrazaAtArk@users.noreply.github.com>
2015-06-24 11:27:23 -0400

    Update AsssetTests.java

--- src/scripts/AsssetTests.java
+++ src/scripts/AsssetTests.java
@@ -1,86 +1,172 @@
 package scripts;
 
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.*;
-import java.util.Arrays;
+	import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 
-import org.hamcrest.core.CombinableMatcher;
-import org.junit.Test;
+import org.junit.*;
 
-public class AsssetTests {
-	 @Test
-	  public void testAssertArrayEquals() {
-	    byte[] expected = "trial".getBytes();
-	    byte[] actual = "trial".getBytes();
-	    org.junit.Assert.assertArrayEquals("failure - byte arrays not same", expected, actual);
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+	public class AssetCoreLoginOnly {
+	  private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://localhost:8012/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	  }
 
 	  @Test
-	  public void testAssertEquals() {
-	    org.junit.Assert.assertEquals("failure - strings are not equal", "text1", "text");
+	  public void testTemp() throws Exception {
+	    driver.get(baseUrl + "/AssetCore/");
+	    driver.findElement(By.name("username")).clear();
+	    driver.findElement(By.name("username")).sendKeys("a");
+	    driver.findElement(By.name("password")).clear();
+	    driver.findElement(By.name("password")).sendKeys("a");
+	    driver.findElement(By.name("submit")).click();
+	    driver.findElement(By.id("topNavUserNameId")).click();
+	    driver.findElement(By.id("signOutOfAssetId")).click();
+	    driver.findElement(By.linkText("sign in")).click();
 	  }
 
-	  @Test
-	  public void testAssertFalse() {
-	    org.junit.Assert.assertFalse("failure - should be false", false);
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
 	  }
 
-	  @Test
-	  public void testAssertNotNull() {
-	    org.junit.Assert.assertNotNull("should not be null", new Object());
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
 	  }
 
-	  @Test
-	  public void testAssertNotSame() {
-	    org.junit.Assert.assertNotSame("should not be same Object", new Object(), new Object());
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
 	  }
 
-	  @Test
-	  public void testAssertNull() {
-	    org.junit.Assert.assertNull("should be null", null);
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
 	  }
+	
+	  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+			//System.setProperty("webdriver.ie.driver", "C:\\Selenium\\IEDriverServer.exe");	
+			//WebDriver driver = new InternetExplorerDriver();
+		     String dbUrl = "jdbc:mysql://localhost:3306/one";
+			 String username = "root";
+			 String password = "password";
+	         String query1    = "select name from pets;";
+	         String query2    = "select owner from pets;";
+	         String query3    = "select species from pets;";
+	         String query4    = "select sex from pets;";
+	         String query5    = "select birth from pets;";         
+	         String query6    = "select death from pets;";      
+	         String query7    = "select * from pets;"; 
+	                 
+			Connection con = DriverManager.getConnection(dbUrl,username,password);
+			 
+			System.out.println("Connected to database");
+			 
+			Statement stmt1 =con.createStatement();
+			Statement stmt2 =con.createStatement();
+			Statement stmt3 =con.createStatement();
+			Statement stmt4 =con.createStatement();
+			Statement stmt5 =con.createStatement();
+			Statement stmt6 =con.createStatement();
+			Statement stmt7 =con.createStatement();
 
-	  @Test
-	  public void testAssertSame() {
-	    Integer aNumber = Integer.valueOf(768);
-	    org.junit.Assert.assertSame("should be same", aNumber, aNumber);
-	  }
+			
+			ResultSet rs1 = stmt1.executeQuery(query1);
+			ResultSet rs2 = stmt2.executeQuery(query2);
+			ResultSet rs3 = stmt3.executeQuery(query3);
+			ResultSet rs4 = stmt4.executeQuery(query4);
+			ResultSet rs5 = stmt5.executeQuery(query5);
+			ResultSet rs6 = stmt6.executeQuery(query6);
+			ResultSet rs7 = stmt7.executeQuery(query7);
 
-	  // JUnit Matchers assertThat
-	  @Test
-	  public void testAssertThatBothContainsString() {
-	    org.junit.Assert.assertThat("albumen", both(containsString("a")).and(containsString("b")));
-	  }
+			
+			  while (rs1.next()) {
+			      String userID = rs1.getString(1);
+				  System.out.println(userID);
+				  }
+			  while (rs2.next()) {
+			      String userFName = rs2.getString(1);
+				  System.out.println(userFName);
+				  }
+			  
+			  while (rs3.next()) {
+			      String userLName = rs3.getString(1);
+				  System.out.println(userLName);
+				  }
 
-	  @Test
-	  public void testAssertThathasItemsContainsString() {
-	    org.junit.Assert.assertThat(Arrays.asList("one", "two", "three"), hasItems("one", "three"));
-	  }
-
-	  @Test
-	  public void testAssertThatEveryItemContainsString() {
-	    org.junit.Assert.assertThat(Arrays.asList(new String[] { "fun", "ban", "net" }), everyItem(containsString("n")));
-	  }
-
-	  // Core Hamcrest Matchers with assertThat
-	  @Test
-	  public void testAssertThatHamcrestCoreMatchers() {
-	    assertThat("good", allOf(equalTo("good"), startsWith("good")));
-	    assertThat("good", not(allOf(equalTo("bad"), equalTo("good"))));
-	    assertThat("good", anyOf(equalTo("bad"), equalTo("good")));
-	    assertThat(7, not(CombinableMatcher.<Integer> either(equalTo(3)).or(equalTo(4))));
-	    assertThat(new Object(), not(sameInstance(new Object())));
-	  }
-
-	  @Test
-	  public void testAssertTrue() {
-	    org.junit.Assert.assertTrue("failure - should be true", true);
-	  }
+			  while (rs4.next()) {
+			      String IDNumber = rs4.getString(1);
+				  System.out.println(IDNumber);
+				  }
+			  
+			  while (rs5.next()) {
+			      String IDNumber = rs5.getString(1);
+				  System.out.println(IDNumber);
+				  }
+			  
+			  while (rs6.next()) {
+			      String IDNumber = rs6.getString(1);
+				  System.out.println(IDNumber);
+				  }
+			  
+			  while (rs7.next()) {
+			      String Name = rs7.getString(1);
+				  System.out.print(Name + ' ');
+			      String Owner = rs7.getString(2);
+				  System.out.print(Owner + ' ');
+			      String Species = rs7.getString(3);
+				  System.out.print(Species + ' ');
+			      String Sex = rs7.getString(4);
+				  System.out.print(Sex + ' ');
+			      String Birth = rs7.getString(5);
+				  System.out.println(Birth + ' ');
+				  }
+			  
+	        con.close();
+			System.out.println("Connection closed");
+		 
+	    
+		}
 	}
 
